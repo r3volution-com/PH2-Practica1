08e5ffbb58aee7d3278c40984ed0feb3f41fd544
themarioga <marioga2512@gmail.com>
2017-04-28 21:27:44 +0200

    yep

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,11 @@
 function ajaxGetRequest(action, callback) {
   	var xhttp = new XMLHttpRequest();
   	xhttp.onreadystatechange = function() {
-   		if (this.readyState == 4 && this.status == 200) {
-   			var obj = JSON.parse(this.responseText);
-   			if (obj.RESULTADO == "ok" && obj.CODIGO == "200"){
-	     		callback(obj);
-	     	} else console.log("Error: la consola no se ha realizado correctamente. "+this.responseText);
+   		if (this.readyState == 4) {
+        if (this.status == 200){
+     			var obj = JSON.parse(this.responseText);
+  	     	callback(obj);
+        } else callback({RESULTADO: "error", CODIGO: this.status});
     	}
  	};
   	xhttp.open("GET", action, true);
@@ -14,12 +14,12 @@ function ajaxGetRequest(action, callback) {
 function ajaxPostRequest(data, action, callback) {
   	var xhttp = new XMLHttpRequest();
   	xhttp.onreadystatechange = function() {
-   		if (this.readyState == 4 && this.status == 200) {
-   			var obj = JSON.parse(this.responseText);
-   			if (obj.RESULTADO == "ok" && obj.CODIGO == "200"){
-	     		callback(obj);
-	     	} else console.log("Error: la consola no se ha realizado correctamente. "+this.responseText);
-    	}
+      if (this.readyState == 4) {
+        if (this.status == 200){
+          var obj = JSON.parse(this.responseText);
+          callback(obj);
+        } else callback({RESULTADO: "error", CODIGO: this.status});
+      }
  	};
   	xhttp.open("POST", action, true);
   	xhttp.send(data);
