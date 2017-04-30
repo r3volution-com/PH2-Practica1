23d9183a42b846a7155d45baccc789610d95fdfa
themarioga <marioga2512@gmail.com>
2017-04-30 13:47:54 +0200

    index

--- js/functions.js
+++ js/functions.js
@@ -27,7 +27,8 @@ function ajaxPostRequest(data, action, callback) {
 function isJson(str) {
     if (str == "") return false;
     try {
-        JSON.parse(str);
+        var json = JSON.parse(str);
+        if (json == null) return false;
     } catch (e) {
         return false;
     }
@@ -58,3 +59,70 @@ function logout(){
   sessionStorage.setItem("login", "");
   location.href=location.href;
 }
+function loadEntries(nEntradas, nPagina){
+  ajaxGetRequest("rest/entrada/", function(res){
+    if (res.RESULTADO == "ok"){
+      var nPaginas = parseInt((res.FILAS.length / nEntradas).toFixed());
+      ajaxGetRequest("rest/entrada/?pag="+(nPagina-1)+"&lpag="+nEntradas, function(res){
+        if (res.RESULTADO == "ok"){
+          let entradas = document.querySelectorAll('.a-container article')
+          for (let entrada of entradas){
+            entrada.remove();
+          }
+          let plantilla = document.querySelector('.articles template');
+          for (let fila of res.FILAS){
+            let elem = plantilla.content.cloneNode(true);
+            
+            elem.querySelector('.aheader a').href = 'entradas.html?id='+fila.id;
+            elem.querySelector('.aheader img').src = 'fotos/'+fila.fichero;
+            elem.querySelector('.aheader img').alt = 'fotos/'+fila.nombre;
+            elem.querySelector('.abody a').href = 'entradas.html?id='+fila.id;
+            elem.querySelector('.abody h3').innerHTML = fila.nombre;
+            elem.querySelector('.abody .descripcion').innerHTML = fila.descripcion;
+            elem.querySelector('.afooter .autor').innerHTML = fila.login;
+            elem.querySelector('.afooter .numcom').innerHTML = fila.ncomentarios+"icono";
+            elem.querySelector('.afooter .numfotos').innerHTML = fila.nfotos+"icono";
+            elem.querySelector('.afooter .fecha').innerHTML = fila.fecha;
+
+            document.querySelector('.a-container').appendChild(elem);
+          }
+        }
+      });
+      if (nPaginas > 1){
+        if (nPagina == 1){
+          document.querySelector('.a-pagination').innerHTML = '<b>'+nPagina+' de '+nPaginas+'</b><a href="#nPag='+(nPagina+1)+'" onclick="loadEntries('+nEntradas+','+(nPagina+1)+')">&gt;</a><a href="#nPag='+nPaginas+'" onclick="loadEntries('+nEntradas+','+nPaginas+')">&gt;&gt;</a>';
+        } else if (nPagina == nPaginas) {
+          document.querySelector('.a-pagination').innerHTML = '<a href="#nPag=1" onclick="loadEntries('+nEntradas+',1)">&lt;&lt;</a><a href="#nPag='+(nPagina-1)+'" onclick="loadEntries('+nEntradas+','+(nPagina-1)+')">&lt;</a><b>'+nPagina+' de '+nPaginas;
+        } else {
+          document.querySelector('.a-pagination').innerHTML = '<a href="#nPag=1" onclick="loadEntries('+nEntradas+',1)">&lt;&lt;</a><a href="#nPag='+(nPagina-1)+'" onclick="loadEntries('+nEntradas+','+(nPagina-1)+')">&lt;</a><b>'+nPagina+' de '+nPaginas+'</b><a href="#nPag='+(nPagina+1)+'" onclick="loadEntries('+nEntradas+','+(nPagina+1)+')">&gt;</a><a href="#nPag='+nPaginas+'" onclick="loadEntries('+nEntradas+','+nPaginas+')">&gt;&gt;</a>';
+        }
+      } else {
+        document.querySelector('.a-pagination').innerHTML = '<b>'+nPagina+' de '+nPaginas+'</b>';
+      }
+    }
+  });
+}
+function loadComments(nComments){
+  ajaxGetRequest("rest/comentario/?u="+nComments, function(res){
+    if (res.RESULTADO == "ok"){
+      let entradas = document.querySelectorAll('.comments div')
+      for (let entrada of entradas){
+        entrada.remove();
+      }
+      let plantilla = document.querySelector('.c-container template');
+      for (let fila of res.FILAS){
+        let elem = plantilla.content.cloneNode(true);
+
+        elem.querySelector('a').href = 'entradas.html?id='+fila.id;
+        elem.querySelector('h3').innerHTML = fila.nombre_entrada;
+        elem.querySelector('b').innerHTML = fila.titulo;
+        elem.querySelector('p').innerHTML = fila.texto;
+        elem.querySelector('.c-author').innerHTML = fila.login;
+        elem.querySelector('.c-date').innerHTML = fila.fecha;
+
+        document.querySelector('.comments').appendChild(elem);
+      }
+    }
+  });
+}
+      
